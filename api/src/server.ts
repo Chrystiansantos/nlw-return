001e0e67565b2c5bf051ea70e2d3dbfead5d16bd
Chrystian Santos <chrystian.santos@irancho.com.br>
2022-05-09 10:29:20 -0300

    config cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,7 +5,9 @@ import { routes } from './routes';
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://nlw-return-two-indol.vercel.app/'
+}))
 app.use(express.json());
 app.use(routes)
 
